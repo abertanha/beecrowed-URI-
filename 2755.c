6e7f69369daa25bec7f5e2c1c494858fffaf4c72
abertanha <110491311+abertanha@users.noreply.github.com>
2024-07-10 13:38:57 -0300

    Create 2755.c

---
+++ 2755.c
@@ -0,0 +1,31 @@
+/*beecrowd | 2755
+Saída 9
+
+Por Roberto A. Costa Jr, UNIFEI BR Brazil
+Timelimit: 1
+
+O seu professor de programação gostaria que você fizesse um programa com as seguintes características:
+
+    Mostre a seguinte frase na tela: "Ro'b'er to\/" (Entre o r e o t tem uma tabulação);
+    Mostre a seguinte frase na tela: (._.) ( l: ) ( .-. ) ( :l ) (._.);
+    Mostre a seguinte frase na tela: (^_-)(-_-) (-_^);
+    Mostre a seguinte frase na tela: ("_") ('.');
+
+Entrada
+
+Não há.
+Saída
+
+O resultado de seu programa deve ser escrito conforme o exemplo de saída.*/
+
+#include <stdio.h>
+ 
+int main() {
+ 
+    printf("\"Ro'b'er\tto\\/\"\n");
+	printf("(._.) ( l: ) ( .-. ) ( :l ) (._.)\n");
+	printf("(^_-) (-_-) (-_^)\n");
+	printf("(\"_\") ('.')\n");
+ 
+    return 0;
+}
